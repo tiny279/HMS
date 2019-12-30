c47d8a4b8655767abec856324b87c157b68380aa
tiny279 <hodiep845@gmail.com>
2019-12-30 10:07:35 +0700

    Installed Entity framework and enabled migrations

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,28 @@
+﻿using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+        }
+
+        public static HMSContext Create()
+        {
+            return new HMSContext();
+        }
+
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+    }
+}
