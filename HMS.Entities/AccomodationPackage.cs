f64ff3942a12c6c75cf8733e8a861d4d40214c47
tiny279 <hodiep845@gmail.com>
2019-12-30 09:46:45 +0700

    Added Basic Entities

---
+++ HMS.Entities/AccomodationPackage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class AccomodationPackage
+    {
+        public int ID { get; set; }
+        public int AccomdationTypeID { get; set; }
+        public AccomodationType AccomodationType { get; set; }
+
+        public string Name { get; set; }
+        public int NoOfRoom { get; set; }
+        public decimal FeePerNight { get; set; }
+    }
+}
